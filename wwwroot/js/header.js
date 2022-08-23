199421406025e2179e512c4eed787bfdf4d53a9c
Pavel Haikdukevich <legobuilder0819@gmail.com>
2022-08-23 11:40:35 +0300

    Внесены правки в целях оптимизации, добавлен выезжающий футер

---
+++ wwwroot/js/header.js
@@ -0,0 +1,15 @@
+﻿let lastScroll = 0
+const defaultOffSet = 1200
+const header = document.querySelector('.header__scroll')
+
+const scrollPostion = () => window.pageYOffset || document.documentElement.scrollTop
+const containHide = () => header.classList.contains('.visible')
+window.addEventListener('scroll', () => {
+    if(scrollPostion() > defaultOffSet){
+        header.classList.add('visible')
+    }
+    if(scrollPostion() < defaultOffSet){
+        header.classList.remove('visible')
+    }
+    lastScroll = scrollPostion()
+})
